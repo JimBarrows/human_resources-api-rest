6663c0873376b797817110fbe88b40c0cadaec89
Jim Barrows <Jim.Barrows@gmail.com>
2020-12-12 08:02:36 -0600

    Initial commit

---
+++ src/main/java/org/erpmicroservices/human_resources/endpoint/rest/human_resourcesapirest/HumanResourcesApiRestApplication.java
@@ -0,0 +1,13 @@
+package org.erpmicroservices.human_resources.endpoint.rest.human_resourcesapirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanResourcesApiRestApplication {
+
+ public static void main(String[] args) {
+	SpringApplication.run(HumanResourcesApiRestApplication.class, args);
+ }
+
+}
---
+++ src/test/java/org/erpmicroservices/human_resources/endpoint/rest/human_resourcesapirest/HumanResourcesApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.erpmicroservices.human_resources.endpoint.rest.human_resourcesapirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanResourcesApiRestApplicationTests {
+
+ @Test
+ void contextLoads() {
+ }
+
+}
